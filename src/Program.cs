7fa59ea85b73047fd54f6dd00e7b69b650d4452d
brunobmc1 <brunomoulie@gmail.com>
2025-05-08 06:28:04 -0300

    teste de commit

--- src/Program.cs
+++ src/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. //
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
