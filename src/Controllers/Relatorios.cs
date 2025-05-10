a73c72e031e8c91faa19f8ddc586dc915d3d43c7
brunobmc1 <brunomoulie@gmail.com>
2025-05-10 07:43:47 -0300

    implementado controlador e view de Relatórios

---
+++ src/Controllers/Relatorios.cs
@@ -0,0 +1,26 @@
+﻿using click_imoveis.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace click_imoveis.Controllers
+{
+    public class RelatoriosController(AppDbContext context) : Controller
+    {
+        private readonly AppDbContext _context = context;
+
+       
+        public IActionResult Index()
+        {
+            var totalDeImoveisCadastrados = _context.Imoveis.Count();
+            ViewBag.totalDeImoveisCadastrados = totalDeImoveisCadastrados;
+
+            var totalDeAnunciosCadastrados = _context.Anuncios.Count();
+            ViewBag.totalDeAnunciosCadastrados = totalDeAnunciosCadastrados;
+
+            var totalDeUsuariosCadastrados = _context.Usuarios.Count();
+            ViewBag.totalDeUsuariosCadastrados = totalDeUsuariosCadastrados;
+
+            return View();
+        }
+    }
+}
